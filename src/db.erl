3570ac6a501bf208e77b2438efa37ee544258743
Philip Crockett <philipmayoo@gmail.com>
2018-04-28 20:45:06 +0200

    Working on database integration, storing messages

---
+++ src/db.erl
@@ -0,0 +1,24 @@
+-module(db).
+-include("db.hrl").
+
+-export([insert_message/1]).
+-export([select_all_messages/0]).
+
+
+insert_message(#s_message{} = Message) ->
+    io:format("Inserting message to Database ~p", [Message]),
+    {ok, Client} = cqerl:get_client({}),
+    {ok, _} = cqerl:run_query(Client, #cql_query{
+        statement = "INSERT INTO message(room_id, id, user, data) VALUES (?, ?, ?, ?)",
+        values = [
+            {room_id, Message#s_message.room_id},
+            {id, Message#s_message.id},
+            {user, Message#s_message.user},
+            {data, Message#s_message.data}
+        ]
+    }).
+    
+select_all_messages() ->
+    {ok, Client} = cqerl:get_client({}),
+    {ok, Result} = cqerl:run_query(Client, "SELECT * FROM message;"),
+    Result.
